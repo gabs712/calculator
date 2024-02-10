10f85a7cd9ef364576b0050ab128bc6183a580b4
gabs <gabs712.g@gmail.com>
2024-02-10 18:53:16 -0300

    Add operation functions

--- scrpit.js
+++ scrpit.js
@@ -1,1 +1,13 @@
 // limit to 9 characters
+function add(n1, n2) {
+  return n1 + n2
+}
+function subtract(n1, n2) {
+  return n1 - n2
+}
+function multiply(n1, n2) {
+  return n1 * n2
+}
+function divide(n1, n2) {
+  return n1 / n2
+}
