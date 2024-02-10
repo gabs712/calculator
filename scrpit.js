111c86cb5dad76f98716a29cb246a18470e7ac68
gabs <gabs712.g@gmail.com>
2024-02-10 20:09:45 -0300

    Add operate function

--- scrpit.js
+++ scrpit.js
@@ -1,4 +1,13 @@
 // limit to 9 characters
+
+
+function operate(n1, operator, n2) {
+  if (operator === '+') add(n1, n2)
+  else if (operator === '-') subtract(n1, n2) 
+  else if (operator === '*') multiply(n1, n2)
+  else if (operator === '/') divide(n1, n2)
+}
+
 function add(n1, n2) {
   return n1 + n2
 }
