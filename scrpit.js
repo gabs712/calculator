97e21aa488a9295f747be2ff1d0b2e9ce2ccfc83
gabs <gabs712.g@gmail.com>
2024-02-11 10:33:28 -0300

    Add clear numbers functinality

--- scrpit.js
+++ scrpit.js
@@ -1,5 +1,36 @@
-// limit to 9 characters
+const VISOR_REGISTER = document.querySelector('.register')
+const VISOR_OPERATION = document.querySelector('.operation')
+const BUTTONS = document.querySelectorAll('.grid button')
 
+const CURRENT_OPERATION = []
+
+BUTTONS.forEach((button) => button.addEventListener('click', ManageOperations))
+
+function ManageOperations(e) {
+  const button = e.target
+  if (button.classList.contains('number')) {
+    populateVisorOperation(button)
+  } else if (button.classList.contains('clear')) {
+    clearNumbers()
+  } else if (button.classList.contains('operator')) {
+
+  }
+}
+
+function populateVisorOperation(button) {
+  if (CURRENT_OPERATION.length < 9) {
+    CURRENT_OPERATION.push(button.value)
+    VISOR_OPERATION.textContent = CURRENT_OPERATION.join('')
+  }
+}
+
+function clearNumbers() {
+  if (CURRENT_OPERATION.length > 0) {
+    CURRENT_OPERATION.splice(0)
+    VISOR_OPERATION.textContent = ''
+    VISOR_REGISTER.textContent = ''
+  }
+}
 
 function operate(n1, operator, n2) {
   if (operator === '+') add(n1, n2)
